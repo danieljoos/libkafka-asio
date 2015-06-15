67e95a080cea1de3719e3f8527f7c79da4a5df6a
Daniel Joos <daniel@joosweb.de>
2015-06-15 10:29:06 +0200

    Cleaned up tests for snappy compression
    Existing tests for decompression have been cleaned up a bit. Added new
    test for compression functionality.

--- test/src/detail/compression_snappy_test.cpp
+++ test/src/detail/compression_snappy_test.cpp
@@ -14,71 +14,80 @@
 using libkafka_asio::Byte;
 using libkafka_asio::Bytes;
 using libkafka_asio::String;
+using libkafka_asio::detail::Compress;
 using libkafka_asio::detail::Decompress;
 
-TEST(CompressionSnappyTest, SimpleDecompress)
+class CompressionSnappyTest : public ::testing::Test
 {
-  const String expected_result = "HelloWorld";
-  String compressed;
-  ::snappy::Compress(expected_result.c_str(), expected_result.size(),
-                     &compressed);
-  Bytes test_data(new Bytes::element_type(compressed.begin(),
-                                          compressed.end()));
-  ASSERT_FALSE(test_data->empty());
+protected:
+  Bytes TestCase1Compressed()
+  {
+    static const unsigned char data[11] =
+    {
+      0x24, 0x14, 0x52, 0x45, 0x50, 0x45, 0x41, 0x54,
+      0x76, 0x06, 0x00
+    };
+    return Bytes(new Bytes::element_type(data, data + sizeof(data)));
+  }
+
+  Bytes TestCase2Compressed()
+  {
+    const unsigned char data[39] =
+    {
+      0x82, 0x53, 0x4e, 0x41, 0x50, 0x50, 0x59, 0x00,
+      0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x01,
+      0x00, 0x00, 0x00, 0x07, 0x05, 0x10, 0x48, 0x65,
+      0x6c, 0x6c, 0x6f, 0x00, 0x00, 0x00, 0x08, 0x06,
+      0x14, 0x20, 0x57, 0x6f, 0x72, 0x6c, 0x64
+    };
+    return Bytes(new Bytes::element_type(data, data + sizeof(data)));
+  }
+
+  Bytes TestCase1Uncompressed()
+  {
+    static const String data = "REPEATREPEATREPEATREPEATREPEATREPEAT";
+    return Bytes(new Bytes::element_type(data.begin(), data.end()));
+  }
+
+  Bytes TestCase2Uncompressed()
+  {
+    static const String data = "Hello World";
+    return Bytes(new Bytes::element_type(data.begin(), data.end()));
+  }
+
+  void AssertBytesEq(const Bytes& expected, const Bytes& actual)
+  {
+    ASSERT_TRUE(expected);
+    ASSERT_TRUE(actual);
+    ASSERT_EQ(expected->size(), actual->size());
+    for (size_t i = 0; i < actual->size(); ++i)
+    {
+      ASSERT_EQ(expected->at(i), actual->at(i));
+    }
+  }
+};
+
+TEST_F(CompressionSnappyTest, SimpleDecompress)
+{
   boost::system::error_code ec;
   using namespace libkafka_asio::constants;
-  Bytes result = Decompress(test_data, kCompressionSnappy, ec);
+  Bytes result = Decompress(TestCase1Compressed(), kCompressionSnappy, ec);
+  Bytes expected_result = TestCase1Uncompressed();
   ASSERT_EQ(libkafka_asio::kErrorSuccess, ec);
-  ASSERT_EQ(expected_result.size(), result->size());
-  ASSERT_STREQ(expected_result.c_str(),
-               String((const char *) &(*result)[0], result->size()).c_str());
+  AssertBytesEq(expected_result, result);
 }
 
-TEST(CompressionSnappyTest, StreamDecompress)
+TEST_F(CompressionSnappyTest, StreamDecompress)
 {
-  // Compress this little test string using two chunks
-  const String expected_result = "Hello World";
-  using boost::asio::streambuf;
-  using boost::asio::buffer_cast;
-  using boost::asio::buffer_size;
-  using libkafka_asio::detail::WriteInt32;
-  using libkafka_asio::Int32;
-  String compressed1, compressed2;
-  ::snappy::Compress(expected_result.c_str(), 5,
-                     &compressed1);
-  ::snappy::Compress(expected_result.c_str() + 5, expected_result.size() - 5,
-                     &compressed2);
-  streambuf compressed_streambuf;
-  std::iostream compressed_stream(&compressed_streambuf);
-  Bytes magic_bytes =
-    libkafka_asio::detail::SnappyCompressionAlgorithm::kSnappyStreamMagic();
-  compressed_stream.write(reinterpret_cast<const char *>(&(*magic_bytes)[0]),
-                          magic_bytes->size());
-  WriteInt32(1, compressed_stream);
-  WriteInt32(1, compressed_stream);
-  WriteInt32(static_cast<Int32>(compressed1.size()), compressed_stream);
-  compressed_stream.write(compressed1.c_str(), compressed1.size());
-  WriteInt32(static_cast<Int32>(compressed2.size()), compressed_stream);
-  compressed_stream.write(compressed2.c_str(), compressed2.size());
-  streambuf::const_buffers_type compressed_data =
-    compressed_streambuf.data();
-  Bytes test_data(new Bytes::element_type(
-    buffer_cast<const Bytes::element_type::value_type *>(compressed_data),
-    buffer_cast<const Bytes::element_type::value_type *>(compressed_data) +
-    buffer_size(compressed_data)
-  ));
-
-  ASSERT_FALSE(test_data->empty());
   boost::system::error_code ec;
   using namespace libkafka_asio::constants;
-  Bytes result = Decompress(test_data, kCompressionSnappy, ec);
+  Bytes result = Decompress(TestCase2Compressed(), kCompressionSnappy, ec);
+  Bytes expected_result = TestCase2Uncompressed();
   ASSERT_EQ(libkafka_asio::kErrorSuccess, ec);
-  ASSERT_EQ(expected_result.size(), result->size());
-  ASSERT_STREQ(expected_result.c_str(),
-               String((const char *) &(*result)[0], result->size()).c_str());
+  AssertBytesEq(expected_result, result);
 }
 
-TEST(CompressionSnappyTest, EmptyDecompress)
+TEST_F(CompressionSnappyTest, EmptyDecompress)
 {
   {
     Bytes test_data;
@@ -96,4 +105,14 @@ TEST(CompressionSnappyTest, EmptyDecompress)
     ASSERT_EQ(libkafka_asio::kErrorCompressionFailed, ec);
     ASSERT_TRUE(!result);
   }
+}
+
+TEST_F(CompressionSnappyTest, SimpleCompress)
+{
+  boost::system::error_code ec;
+  using namespace libkafka_asio::constants;
+  Bytes result = Compress(TestCase1Uncompressed(), kCompressionSnappy, ec);
+  Bytes expected_result = TestCase1Compressed();
+  ASSERT_EQ(libkafka_asio::kErrorSuccess, ec);
+  AssertBytesEq(expected_result, result);
 }
