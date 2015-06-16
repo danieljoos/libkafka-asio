415c4d714ee621fce0c674a596419686b4d426c1
Daniel Joos <daniel@joosweb.de>
2015-06-16 10:38:35 +0200

    GZip compression implementation
    GZip compression using zlib

--- lib/libkafka_asio/detail/impl/compression_gz.h
+++ lib/libkafka_asio/detail/impl/compression_gz.h
@@ -12,6 +12,8 @@
 
 #include <zlib.h>
 
+#include <algorithm>
+
 #include <libkafka_asio/constants.h>
 #include <libkafka_asio/error.h>
 #include <libkafka_asio/primitives.h>
@@ -24,8 +26,44 @@ namespace detail
 inline Bytes GZIPCompressionAlgorithm::Compress(
   const Bytes& data, boost::system::error_code& ec)
 {
-  ec = kErrorCompressionNotAvailable;
-  return Bytes();
+  static const size_t kBufferSize = 32 * 1024;
+  static const int kGZIPWindowBits = 15+16;
+
+  // Initialize a new zlib stream
+  ::z_stream gz = {};
+  ::gz_header header = {};
+  if (::deflateInit2(&gz, Z_DEFAULT_COMPRESSION, Z_DEFLATED, kGZIPWindowBits, 
+                     8, Z_DEFAULT_STRATEGY) != Z_OK)
+  {
+    ec = kErrorCompressionFailed;
+    return Bytes();
+  }
+  gz.next_in = reinterpret_cast< ::Bytef *>(&(*data)[0]);
+  gz.avail_in = (::uInt) data->size();
+  Bytes out(new Bytes::element_type());
+  int ret = Z_OK;
+  // Deflate
+  while (ret == Z_OK)
+  {
+    size_t pos = out->size();
+    out->resize(out->size() + kBufferSize);
+    gz.next_out = reinterpret_cast< ::Bytef *>(&(*out)[pos]);
+    gz.avail_out = kBufferSize;
+    ret = ::deflate(&gz, Z_FINISH);
+  }
+  if (gz.total_out < out->size())
+  {
+    out->resize(gz.total_out);
+  }
+  ::deflateEnd(&gz);
+  if (ret != Z_STREAM_END)
+  {
+    ec = kErrorCompressionFailed;
+    return Bytes();
+  }
+
+  ec = kErrorSuccess;
+  return out;
 }
 
 inline Bytes GZIPCompressionAlgorithm::Decompress(
