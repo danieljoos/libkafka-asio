00888a9c33d51e89a8277b5ea2bc6d91414cfe81
Daniel Joos <daniel@joosweb.de>
2015-08-05 16:41:39 +0200

    Added `ConnectionService`
    Started to move the `Connection` logic into a Boost asio service
    implementation. This allows getting rid of the _shared state_ and the
    `kStateDestroyed`. It uses a `weak_ptr` for that.

---
+++ lib/libkafka_asio/detail/connection_service.h
@@ -0,0 +1,242 @@
+//
+// detail/connection_service.h
+// ---------------------------
+//
+// Copyright (c) 2015 Daniel Joos
+//
+// Distributed under MIT license. (See file LICENSE)
+//
+
+#ifndef CONNECTION_SERVICE_H_BD87CD55_1B15_4F19_AE8C_B78C42BBE950
+#define CONNECTION_SERVICE_H_BD87CD55_1B15_4F19_AE8C_B78C42BBE950
+
+#include <deque>
+#include <boost/asio.hpp>
+#include <boost/function.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/enable_shared_from_this.hpp>
+#include <boost/system/error_code.hpp>
+#include <libkafka_asio/connection_configuration.h>
+
+namespace libkafka_asio
+{
+namespace detail
+{
+
+template<typename Service>
+class BasicConnectionService :
+  public boost::asio::io_service::service
+{
+public:
+
+  // Service implementation type
+  typedef Service BaseType;
+  typedef boost::shared_ptr<Service> ImplementationType;
+  typedef ImplementationType implementation_type;
+
+  // Unique service identifier
+  static boost::asio::io_service::id id;
+
+  // Construct a new connection service
+  explicit BasicConnectionService(boost::asio::io_service& io_service) :
+    boost::asio::io_service::service(io_service)
+  {
+  }
+
+  // Destroy all user-defined handler objects owned by the service
+  void shutdown_service()
+  {
+  }
+
+  // Construct a new connection service implementation
+  void construct(implementation_type& impl)
+  {
+    impl.reset(new Service(get_io_service()));
+  }
+
+  // Destroy a connection service implementation
+  void destroy(implementation_type& impl)
+  {
+    impl.reset();
+  }
+};
+
+//
+// Connection Service Implementation Class
+//
+// This class actually holds all objects, required for a single connection,
+// such as the actual socket object as well as the write/read queues.
+// It holds three different states: 1) connection state, 2) write operation
+// state and 3) the read operation state. It manages a write and read queue
+// in parallel, so there will always be one write and one read operation
+// pending on the socket.
+//
+class ConnectionServiceImpl :
+  public boost::enable_shared_from_this<ConnectionServiceImpl>
+{
+public:
+  // Error codes use this type
+  typedef boost::system::error_code ErrorCodeType;
+
+  // Handler type definition for connection operations
+  typedef boost::function<void(const ErrorCodeType&)> ConnectionHandlerType;
+
+  // Handler type definitions for transmission operations
+  typedef boost::function<void(const ErrorCodeType&, size_t)> TxHandlerType;
+
+  // Handler type deduction template depending on the type of request
+  template<typename TRequest>
+  struct Handler
+  {
+    typedef typename TRequest::ResponseType::OptionalType ResponseType;
+    typedef boost::function<void(const ErrorCodeType&,
+                                 const ResponseType&)> Type;
+  };
+
+private:
+  typedef boost::asio::ip::tcp::socket SocketType;
+  typedef boost::asio::deadline_timer DeadlineTimerType;
+  typedef boost::asio::ip::tcp::resolver ResolverType;
+  typedef boost::shared_ptr<boost::asio::streambuf> StreambufType;
+
+  enum ConnectionState
+  {
+    kConnectionStateClosed = 0,
+    kConnectionStateConnecting,
+    kConnectionStateConnected
+  };
+
+  enum TxState
+  {
+    kTxStateIdle = 0,
+    kTxStateBusy
+  };
+
+  struct QueueItem
+  {
+    StreambufType buffer;
+    TxHandlerType handler;
+  };
+
+  typedef std::deque<QueueItem> WriteQueue;
+  typedef std::deque<QueueItem> ReadQueue;
+
+public:
+
+  // Construct a new connection service object
+  ConnectionServiceImpl(boost::asio::io_service& io_service);
+
+  // Sets the connection configuration
+  void set_configuration(const ConnectionConfiguration& configuration);
+
+  // Closes the connection to the Kafka server.
+  void Close();
+
+  // Asynchronous connection to the specified Kafka server
+  void AsyncConnect(const std::string& host,
+                    const std::string& service,
+                    const ConnectionHandlerType& handler);
+
+  // Asynchronously sends the given request to the connected Kafka server.
+  template<typename TRequest>
+  void AsyncRequest(const TRequest& request,
+                    const typename Handler<TRequest>::Type& handler);
+
+// Internal helper functions:
+private:
+  
+  // Resets the operation timeout
+  void SetDeadline();
+
+  // Serialize the given request to the Kafka wire format
+  template<typename TRequest>
+  StreambufType Serialize(const TRequest& request);
+
+// Flow functions:
+private:
+  
+  // Push the request + handler to the write queue
+  template<typename TRequest>
+  void EnqueueRequest(const TRequest& request,
+                      const typename Handler<TRequest>::Type& handler);
+
+  // Push the given handler to the read queue
+  template<typename TRequest>
+  void EnqueueResponse(StreambufType buffer,
+                       const typename Handler<TRequest>::Type& handler);
+
+  // Take the next request from the write queue
+  void NextRequest();
+
+  // Work on the next response from the read queue
+  void NextResponse();
+
+  // Schedule the send operation for the given write queue item
+  void SendRequest(const QueueItem& item);
+
+  // Schedule the receive operation for the given read queue item
+  void ReceiveResponse(const QueueItem& item);
+
+// Handler functions:
+private:
+
+  // Handle async resolve operation
+  void HandleAsyncResolve(const ErrorCodeType& error,
+                          const ResolverType::iterator& iter,
+                          const ConnectionHandlerType& handler);
+
+  // Handle async connect operations
+  void HandleAsyncConnect(const ErrorCodeType& error,
+                          const ResolverType::iterator& iter,
+                          const ConnectionHandlerType& handler);
+
+  // Handle async request write operations
+  template<typename TRequest>
+  void HandleAsyncRequestWrite(
+    const ErrorCodeType& error,
+    size_t bytes_transferred,
+    StreambufType buffer,
+    const typename Handler<TRequest>::Type& handler,
+    bool response_expected);
+
+  // Handle async read of response size
+  template<typename TRequest>
+  void HandleAsyncResponseSizeRead(
+    const ErrorCodeType& error,
+    size_t bytes_transferred,
+    StreambufType buffer,
+    const typename Handler<TRequest>::Type& handler);
+
+  // Handle async read of response body
+  template<typename TRequest>
+  void HandleAsyncResponseRead(
+    const ErrorCodeType& error,
+    size_t bytes_transferred,
+    StreambufType buffer,
+    const typename Handler<TRequest>::Type& handler);
+
+  // Handle operation timeout
+  void HandleDeadline(const ErrorCodeType& error);
+
+private:
+  ConnectionConfiguration configuration_;
+  WriteQueue write_queue_;
+  ReadQueue read_queue_;
+  ConnectionState connection_state_;
+  TxState write_state_;
+  TxState read_state_;
+
+  boost::asio::io_service& io_service_;
+  SocketType socket_;
+  DeadlineTimerType deadline_;
+  ResolverType resolver_;
+};
+
+typedef BasicConnectionService<ConnectionServiceImpl> ConnectionService;
+
+}  // namespace detail
+}  // namespace libkafka_asio
+
+#include <libkafka_asio/detail/impl/connection_service.h>
+
+#endif  // CONNECTION_SERVICE_H_BD87CD55_1B15_4F19_AE8C_B78C42BBE950
---
+++ lib/libkafka_asio/detail/impl/connection_service.h
@@ -0,0 +1,406 @@
+//
+// detail/impl/connection_service.h
+// --------------------------------
+//
+// Copyright (c) 2015 Daniel Joos
+//
+// Distributed under MIT license. (See file LICENSE)
+//
+
+#ifndef CONNECTION_SERVICE_H_5310FB3D_9D78_4C52_AE32_EB71E000F4ED
+#define CONNECTION_SERVICE_H_5310FB3D_9D78_4C52_AE32_EB71E000F4ED
+
+#include <boost/bind.hpp>
+#include <libkafka_asio/detail/weak_impl_handler.h>
+
+namespace libkafka_asio
+{
+namespace detail
+{
+
+template<typename Service>
+boost::asio::io_service::id BasicConnectionService<Service>::id;
+
+inline ConnectionServiceImpl::ConnectionServiceImpl(
+  boost::asio::io_service& io_service) :
+  connection_state_(kConnectionStateClosed),
+  write_state_(kTxStateIdle),
+  read_state_(kTxStateIdle),
+  io_service_(io_service),
+  socket_(io_service),
+  deadline_(io_service),
+  resolver_(io_service)
+{
+}
+
+inline void ConnectionServiceImpl::set_configuration(
+  const ConnectionConfiguration& configuration)
+{
+  configuration_ = configuration;
+}
+
+inline void ConnectionServiceImpl::Close()
+{
+  connection_state_ = kConnectionStateClosed;
+  boost::system::error_code ec;
+  resolver_.cancel();
+  socket_.shutdown(SocketType::shutdown_both, ec);
+  socket_.close(ec);
+  deadline_.cancel(ec);
+}
+
+inline void ConnectionServiceImpl::AsyncConnect(
+  const std::string& host,
+  const std::string& service,
+  const ConnectionServiceImpl::ConnectionHandlerType& handler)
+{
+  if (connection_state_ != kConnectionStateClosed)
+  {
+    io_service_.post(boost::bind(handler, kErrorAlreadyConnected));
+    return;
+  }
+  ResolverType::query query(host, service);
+  resolver_.async_resolve(
+    query,
+    WeakImpl<ConnectionServiceImpl>::ResolveHandler(
+      shared_from_this(),
+      boost::bind(
+        &ConnectionServiceImpl::HandleAsyncResolve, this,
+        boost::asio::placeholders::error,
+        boost::asio::placeholders::iterator,
+        handler)));
+  connection_state_ = kConnectionStateConnecting;
+  SetDeadline();
+}
+
+template<typename TRequest>
+inline void ConnectionServiceImpl::AsyncRequest(
+  const TRequest& request,
+  const typename ConnectionServiceImpl::Handler<TRequest>::Type& handler)
+{
+  io_service_.post(
+    WeakImpl<ConnectionServiceImpl>::NullaryHandler(
+      shared_from_this(),
+      boost::bind(
+        &ConnectionServiceImpl::EnqueueRequest<TRequest>, this,
+        request,
+        handler)));
+}
+
+inline void ConnectionServiceImpl::SetDeadline()
+{
+  using boost::posix_time::milliseconds;
+  deadline_.expires_from_now(milliseconds(configuration_.socket_timeout));
+  deadline_.async_wait(
+    WeakImpl<ConnectionServiceImpl>::DeadlineHandler(
+      shared_from_this(),
+      boost::bind(
+        &ConnectionServiceImpl::HandleDeadline, this,
+        boost::asio::placeholders::error)));
+}
+
+template<typename TRequest>
+inline ConnectionServiceImpl::StreambufType ConnectionServiceImpl::Serialize(
+  const TRequest& request)
+{
+  StreambufType buffer(new StreambufType::element_type());
+  std::ostream os(buffer.get());
+  detail::WriteRequest(request, configuration_.client_id, os);
+  return buffer;
+}
+
+template<typename TRequest>
+inline void ConnectionServiceImpl::EnqueueRequest(
+  const TRequest& request,
+  const typename ConnectionServiceImpl::Handler<TRequest>::Type& handler)
+{
+  if (connection_state_ != kConnectionStateConnected)
+  {
+    typename TRequest::ResponseType::OptionalType empty_response;
+    io_service_.post(boost::bind(handler, kErrorNotConnected, empty_response));
+    return;
+  }
+  QueueItem item;
+  item.buffer = Serialize(request);
+  bool response_expected = request.ResponseExpected();
+  item.handler =
+    boost::bind(
+      &ConnectionServiceImpl::HandleAsyncRequestWrite<TRequest>, this,
+      boost::asio::placeholders::error,
+      boost::asio::placeholders::bytes_transferred,
+      item.buffer,
+      handler,
+      response_expected);
+  if (write_state_ == kTxStateIdle)
+  {
+    if (write_queue_.empty())
+    {
+      SendRequest(item);
+    }
+    else
+    {
+      write_queue_.push_back(item);
+      NextRequest();
+    }
+  }
+  else
+  {
+    write_queue_.push_back(item);
+  }
+}
+
+template<typename TRequest>
+inline void ConnectionServiceImpl::EnqueueResponse(
+  ConnectionServiceImpl::StreambufType buffer,
+  const typename ConnectionServiceImpl::Handler<TRequest>::Type& handler)
+{
+  if (connection_state_ != kConnectionStateConnected)
+  {
+    typename TRequest::ResponseType::OptionalType empty_response;
+    io_service_.post(boost::bind(handler, kErrorNotConnected, empty_response));
+    while (!read_queue_.empty())
+    {
+      QueueItem& item = read_queue_.front();
+      io_service_.post(boost::bind(item.handler, kErrorNotConnected, 0));
+      read_queue_.pop_front();
+    }
+    return;
+  }
+  QueueItem item;
+  item.buffer = buffer;
+  item.handler =
+    boost::bind(
+      &ConnectionServiceImpl::HandleAsyncResponseSizeRead<TRequest>, this,
+      boost::asio::placeholders::error,
+      boost::asio::placeholders::bytes_transferred,
+      buffer,
+      handler);
+  if (read_state_ == kTxStateIdle)
+  {
+    if (read_queue_.empty())
+    {
+      ReceiveResponse(item);
+    }
+    else
+    {
+      read_queue_.push_back(item);
+      NextResponse();
+    }
+  }
+  else
+  {
+    read_queue_.push_back(item);
+  }
+}
+
+inline void ConnectionServiceImpl::NextRequest()
+{
+  if (write_state_ == kTxStateBusy || write_queue_.empty())
+  {
+    return;
+  }
+  QueueItem& item = write_queue_.front();
+  // TODO: Add auto-connect code here!
+  SendRequest(item);
+  write_queue_.pop_front();
+}
+
+inline void ConnectionServiceImpl::NextResponse()
+{
+  if (read_state_ == kTxStateBusy || read_queue_.empty())
+  {
+    return;
+  }
+  QueueItem& item = read_queue_.front();
+  ReceiveResponse(item);
+  read_queue_.pop_front();
+}
+
+inline void ConnectionServiceImpl::SendRequest(
+  const ConnectionServiceImpl::QueueItem& item)
+{
+  boost::asio::async_write(
+    socket_, *item.buffer, 
+    WeakImpl<ConnectionServiceImpl>::WriteHandler(
+      shared_from_this(),
+      item.handler));
+  write_state_ = kTxStateBusy;
+  //SetDeadline();  // TODO: Need a second timer!
+}
+
+inline void ConnectionServiceImpl::ReceiveResponse(
+  const ConnectionServiceImpl::QueueItem& item)
+{
+  boost::asio::async_read(
+    socket_,
+    item.buffer->prepare(sizeof(Int32)),
+    boost::asio::transfer_exactly(sizeof(Int32)),
+    WeakImpl<ConnectionServiceImpl>::ReadHandler(
+      shared_from_this(), 
+      item.handler));
+  read_state_ = kTxStateBusy;
+  //SetDeadline();  // TODO: Need a second timer!
+}
+
+inline void ConnectionServiceImpl::HandleAsyncResolve(
+  const ConnectionServiceImpl::ErrorCodeType& error,
+  const ConnectionServiceImpl::ResolverType::iterator& iter,
+  const ConnectionServiceImpl::ConnectionHandlerType& handler)
+{
+  if (error)
+  {
+    io_service_.post(boost::bind(handler, error));
+    Close();
+    return;
+  }
+  boost::asio::async_connect(
+    socket_, iter,
+    WeakImpl<ConnectionServiceImpl>::ConnectHandler(
+      shared_from_this(),
+      boost::bind(
+        &ConnectionServiceImpl::HandleAsyncConnect, this,
+        boost::asio::placeholders::error,
+        boost::asio::placeholders::iterator,
+        handler)));
+  SetDeadline();
+}
+
+inline void ConnectionServiceImpl::HandleAsyncConnect(
+  const ConnectionServiceImpl::ErrorCodeType& error,
+  const ConnectionServiceImpl::ResolverType::iterator&,
+  const ConnectionServiceImpl::ConnectionHandlerType& handler)
+{
+  if (error)
+  {
+    Close();
+  }
+  else
+  {
+    connection_state_ = kConnectionStateConnected;
+    deadline_.cancel();
+  }
+  io_service_.post(boost::bind(handler, error));
+}
+
+template<typename TRequest>
+inline void ConnectionServiceImpl::HandleAsyncRequestWrite(
+  const ConnectionServiceImpl::ErrorCodeType& error,
+  size_t bytes_transferred,
+  ConnectionServiceImpl::StreambufType buffer,
+  const typename ConnectionServiceImpl::Handler<TRequest>::Type& handler,
+  bool response_expected)
+{
+  write_state_ = kTxStateIdle;
+  typedef typename TRequest::ResponseType::OptionalType OptionalResponse;
+  if (error)
+  {
+    OptionalResponse empty_response;
+    io_service_.post(boost::bind(handler, error, empty_response));
+    Close();
+    return;
+  }
+  if (!response_expected)
+  {
+    OptionalResponse empty_response;
+    io_service_.post(boost::bind(handler, error, empty_response));
+    deadline_.cancel();
+    NextRequest();
+    return;
+  }
+  buffer->consume(buffer->size());
+  EnqueueResponse<TRequest>(buffer, handler);
+}
+
+template<typename TRequest>
+inline void ConnectionServiceImpl::HandleAsyncResponseSizeRead(
+  const ConnectionServiceImpl::ErrorCodeType& error,
+  size_t bytes_transferred,
+  ConnectionServiceImpl::StreambufType buffer,
+  const typename ConnectionServiceImpl::Handler<TRequest>::Type& handler)
+{
+  typedef typename TRequest::ResponseType::OptionalType OptionalResponse;
+  if (error)
+  {
+    OptionalResponse empty_response;
+    io_service_.post(boost::bind(handler, error, empty_response));
+    Close();
+    return;
+  }
+  buffer->commit(bytes_transferred);
+  std::istream is(buffer.get());
+  Int32 size = detail::ReadInt32(is);
+  if (size >= configuration_.message_max_bytes)
+  {
+    OptionalResponse empty_response;
+    io_service_.post(boost::bind(handler, kErrorMessageSizeTooLarge,
+                                 empty_response));
+    Close();
+    return;
+  }
+  boost::asio::async_read(
+    socket_,
+    buffer->prepare(size),
+    boost::asio::transfer_exactly(size),
+    WeakImpl<ConnectionServiceImpl>::ReadHandler(
+      shared_from_this(),
+      boost::bind(
+        &ConnectionServiceImpl::HandleAsyncResponseRead<TRequest>, this,
+        boost::asio::placeholders::error,
+        boost::asio::placeholders::bytes_transferred,
+        buffer,
+        handler)));
+  //SetDeadline();  // TODO: Need a second timer!
+}
+
+template<typename TRequest>
+inline void ConnectionServiceImpl::HandleAsyncResponseRead(
+  const ConnectionServiceImpl::ErrorCodeType& error,
+  size_t bytes_transferred,
+  ConnectionServiceImpl::StreambufType buffer,
+  const typename ConnectionServiceImpl::Handler<TRequest>::Type& handler)
+{
+  typedef typename TRequest::ResponseType::OptionalType OptionalResponse;
+  if (error)
+  {
+    OptionalResponse empty_response;
+    io_service_.post(boost::bind(handler, error, empty_response));
+    Close();
+    return;
+  }
+  read_state_ = kTxStateIdle;
+  deadline_.cancel();
+  buffer->commit(bytes_transferred);
+  std::istream is(buffer.get());
+  typename TRequest::MutableResponseType response;
+  boost::system::error_code ec;
+  detail::ReadResponse(is, response, ec);
+  if (ec)
+  {
+    OptionalResponse empty_response;
+    io_service_.post(boost::bind(handler, ec, empty_response));
+  }
+  else
+  {
+    io_service_.post(boost::bind(handler, ec, response.response()));
+  }
+  NextRequest();
+}
+
+inline void ConnectionServiceImpl::HandleDeadline(
+  const ConnectionServiceImpl::ErrorCodeType& error)
+{
+  if (error)
+  {
+    return;
+  }
+  if (deadline_.expires_at() <= DeadlineTimerType::traits_type::now())
+  {
+    deadline_.expires_at(boost::posix_time::pos_infin);
+    Close();
+  }
+}
+
+}  // namespace detail
+}  // namespace libkafka_asio
+
+#endif  // CONNECTION_SERVICE_H_5310FB3D_9D78_4C52_AE32_EB71E000F4ED
